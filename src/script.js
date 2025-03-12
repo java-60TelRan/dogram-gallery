a0f894581fd065c581b702e5c86079f507c13f52
Yuragranovsky <yuragranovsky@gmail.com>
2025-03-12 20:36:18 +0200

    setDetails with this

--- src/script.js
+++ src/script.js
@@ -2,16 +2,14 @@ const detailedImage = document.querySelector(".detailedContainer--image");
 const detailedTitle = document.querySelector(".detailedContainer--title");
 const galleryImages = document.querySelectorAll(".gallery--item_image");
 for (let i = 0; i < galleryImages.length; i++) {
-  galleryImages[i].addEventListener("click", function () {
-    setDetails(galleryImages[i]);
-  });
+  galleryImages[i].addEventListener("click", setDetails.bind( galleryImages[i]));
 }
-function setDetails(galleryImage) {
-  let image = galleryImage.getAttribute("data-detailed-image");
+function setDetails() {
+  let image = this.getAttribute("data-detailed-image");
   detailedImage.src = "";
   detailedImage.src = image;
   detailedTitle.innerHTML =
-    galleryImage.getAttribute("data-detailed-title") +
+    this.getAttribute("data-detailed-title") +
     '<span class="for_ellipsis">...</span>';
   animate();
 }
